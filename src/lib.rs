870a67d4e9e46f7106ba19fdc3d67829419cbbf4
Ibe Arua <stanarua@aol.com>
2026-06-05 23:45:48 +0100

    Assignment completed

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,33 @@ use hex::{decode, encode};
 
 pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
     // TODO: Decode hex string into Vec<u8>, return error string on failure
+    let decoded_hex = decode(hex_str).map_err(|e| format!("Hex decoding error: {}", e))?;
+    Ok(decoded_hex)
 }
 
 pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
     // TODO: Reverse the byte order of input slice and return as Vec<u8>
+    bytes.iter().rev().cloned().collect()
 }
 
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
     // TODO: Implement conversion of bytes slice to hex string
+    encode(bytes)   
 }
 
 pub fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, hex::FromHexError> {
     // TODO: Implement conversion of hex string to bytes vector
+    decode(hex)
 }
 
 pub fn swap_endian_u32(num: u32) -> [u8; 4] {
     // TODO: Implement little-endian byte swap for u32
+    num.to_le_bytes()
 }
 
 pub fn parse_satoshis(input: &str) -> Result<u64, String> {
     // TODO: Parse input string to u64, return error string if invalid
+    input.parse::<u64>().map_err(|_| "Invalid satoshi amount".to_string())
 }
 
 pub enum ScriptType {
@@ -32,13 +39,19 @@ pub enum ScriptType {
 
 pub fn classify_script(script: &[u8]) -> ScriptType {
     // TODO: Match script pattern and return corresponding ScriptType
+    match script {
+        [0x76, 0xa9, ..] => ScriptType::P2PKH, // OP_DUP OP_HASH160 <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
+        [0x00, 0x14, ..] => ScriptType::P2WPKH, // OP_0 <20-byte pubKeyHash>
+        _ => ScriptType::Unknown,
+    }
 }
 
 // TODO: complete Outpoint tuple struct
-pub struct Outpoint();
+pub struct Outpoint(pub String, pub u32);
 
 pub fn read_pushdata(script: &[u8]) -> &[u8] {
     // TODO: Return the pushdata portion of the script slice (assumes pushdata starts at index 2)
+    &script[2..]
 }
 
 pub trait Wallet {
@@ -52,18 +65,22 @@ pub struct TestWallet {
 impl Wallet for TestWallet {
     fn balance(&self) -> u64 {
         // TODO: Return the wallet's confirmed balance
+        self.confirmed
     }
 }
 
 pub fn apply_fee(balance: &mut u64, fee: u64) {
     // TODO: Subtract fee from mutable balance reference
+    *balance -= fee;
 }
 
 pub fn move_txid(txid: String) -> String {
     // TODO: Return formatted string including the txid for display or logging
+    format!("txid: {}", txid)
 }
 
 // TODO: Add necessary derive traits
+#[derive(Debug, PartialEq)]
 pub enum Opcode {
     OpChecksig,
     OpDup,
@@ -73,10 +90,16 @@ pub enum Opcode {
 impl Opcode {
     pub fn from_byte(byte: u8) -> Result<Self, String> {
         // TODO: Implement mapping from byte to Opcode variant
+        match byte {
+            0xac => Ok(Opcode::OpChecksig), // OP_CHECKSIG
+            0x76 => Ok(Opcode::OpDup),      // OP_DUP
+            _ => Err(format!("Invalid opcode: 0x{:02x}", byte)),
+        }
     }
 }
 
 // TODO: Add necessary derive traits
+#[derive(Debug, PartialEq, Clone)]
 pub struct UTXO {
     pub txid: Vec<u8>,
     pub vout: u32,
@@ -85,4 +108,6 @@ pub struct UTXO {
 
 pub fn consume_utxo(utxo: UTXO) -> UTXO {
     // TODO: Implement UTXO consumption logic (if any)
+    utxo
+
 }
